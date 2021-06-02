ad990cff2af488b1e3ae895eea430ba726d3ac69
Lea <leandro.gramatico@bue.edu.ar>
2021-06-01 23:05:12 -0300

    mi primer commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const array = [1, 8, 2, 32, 9, 7, 4];
+
+// Función de comparación para ordenación natural
+const fc = function (a, b) {
+  return b -a;
+};
+
+
+
+
+
+console.log(array.sort());
